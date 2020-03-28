8f61a83209c55e67363d01d67d6b6aebb5f4ab06
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-28 17:23:21 +0530

    more about useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
-
-// import Tweet from './Tweet';
+import Tweet from './Tweet';
 
 // const sayHello =  () => {
 //   console.log("Hello");
 // };
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [isRed,setRed] =useState(false);
+  // const [count,setCount] = useState(0);
+  // const [isRed,setRed] =useState(false);
 
-  const increase = () => {
-    setCount(count+1);  
-    setRed(!isRed);
-  };
-  const reset = () => {
-    setCount(0);
-  };
+  // const increase = () => {
+  //   setCount(count+1);  
+  //   setRed(!isRed);
+  // };
+  // const reset = () => {
+  //   setCount(0);
+  // };
+  const [users, setUsers] = useState([
+    {name: "Injamul", message: "Hi there", likes: "4k"},
+    {name: "Injamul", message: "Hi there", likes: "4k"},
+    {name: "Injamul", message: "Hi there", likes: "4k"},
+    {name: "Injamul", message: "Hi there", likes: "4k"},
+  ]);
+  
 
   return (
     <div className="App">
@@ -25,9 +31,13 @@ function App() {
       <Tweet name="Injamul" message="first post." likes="100" />
       <Tweet name="Injamul" message="first post." likes="100" />
       <Tweet name="Injamul" message="first post." likes="100" /> */}
-      <h1 className={isRed ? "red" : ""}>{count}</h1>
+      {/* <h1 className={isRed ? "red" : ""}>{count}</h1>
       <button onClick={increase}>Increase</button>
-      <button onClick={reset}>reset</button>
+      <button onClick={reset}>reset</button> */}
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} likes={user.likes} />
+      ))}
+      
     </div>
   );
 }
