02a9f22392f198aa708a1b08b00f1a1281119f6d
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-28 17:09:32 +0530

    new way to use State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// import Tweet from './Tweet';
+
+// const sayHello =  () => {
+//   console.log("Hello");
+// };
+
 function App() {
+  const [count,setCount] = useState(0);
+  const [isRed,setRed] =useState(false);
+
+  const increase = () => {
+    setCount(count+1);  
+    setRed(!isRed);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Tweet name="Injamul" message="first post." likes="100" />
+      <Tweet name="Injamul" message="first post." likes="100" />
+      <Tweet name="Injamul" message="first post." likes="100" />
+      <Tweet name="Injamul" message="first post." likes="100" /> */}
+      <h1 className={isRed ? "red" : ""}>{count}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
