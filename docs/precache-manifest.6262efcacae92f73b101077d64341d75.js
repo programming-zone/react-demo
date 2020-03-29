afc1e8c721330241ef23c3e9f1a3450ba8b75569
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-30 00:14:55 +0530

    Added deployment

---
+++ docs/precache-manifest.6262efcacae92f73b101077d64341d75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbd2f81c343fa49aeac625aabee4f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a931e483c84917b7f6e4",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "351e072daa0e355d6b53",
+    "url": "/static/css/main.ae5f31f1.chunk.css"
+  },
+  {
+    "revision": "a931e483c84917b7f6e4",
+    "url": "/static/js/2.2482fd2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2482fd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351e072daa0e355d6b53",
+    "url": "/static/js/main.4b3e2e67.chunk.js"
+  },
+  {
+    "revision": "535d1337d482cd083e58",
+    "url": "/static/js/runtime-main.d460aa75.js"
+  }
+]);
