816efb333d3b293d6fa51b79504a3f3c0aff97ff
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-29 22:14:53 +0530

    add reset button and fix state issues

--- src/counter.js
+++ src/counter.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = { 
-        count: this.props.value,
-     }
 
      formatCount() {
-         const {count} = this.state;
-         return count === 0 ? "Zero" : count;
+         const {value} = this.props.counter;
+         return value === 0 ? "Zero" : value;
      };
      getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
      }
-
-     handleIncrement = () => {
-         console.log("Clicked");
-         this.setState({count: this.state.count + 1});
-     };
-
 
     render() { 
 
         return ( 
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary byn-sm" onClick={this.handleIncrement}>Increment</button>
-                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)} >Delete</button>
+                <button className="btn btn-secondary byn-sm" onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)} >Delete</button>
             </div>
          );
     }
