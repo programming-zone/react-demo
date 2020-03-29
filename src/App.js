a6e7e78bdf6541f99f78852ae70cba6bae3761a5
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-29 22:54:10 +0530

    lifting state up and add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './navbar';
 import Counters from './Counters';
 
 class App extends Component {
-  state ={}
+  state = { 
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ]
+   }
+   handleIncrement = (counter) => {
+     const counters =[...this.state.counters];
+     const index = counters.indexOf(counter);
+     counters[index] = {...counter};
+     counters[index].value++;
+     this.setState({counters});
+   };
+   handleReset = () => {
+     const counters = this.state.counters.map(c => {
+       c.value =0;
+       return c;
+     });
+     this.setState({counters});
+   };
+
+   handleDelete = (counterId) => {
+     const counters = this.state.counters.filter(c => c.id !== counterId);
+     this.setState({counters});
+   };
+
   render() { 
-    return ( 
-    <div className="App">
-      <Counters />
-    </div>
+    return (
+      <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters 
+        counters={this.state.counters}
+        onReset={this.handleReset}
+        onIncrement={this.handleIncrement}
+        onDelete={this.handleDelete} />
+      </main>
+      </React.Fragment> 
      );
   }
 }
