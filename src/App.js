0b4b57b8398c9e4c6f44600f5471aadaf08c8229
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-29 21:34:53 +0530

    add delete button

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Counter from './counter';
 class App extends Component {
   state = { 
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ]
    }
+
+   handleDelete = (counterId) => {
+     const counters = this.state.counters.filter(c => c.id !== counterId);
+     this.setState({counters});
+   };
+
   render() { 
     return ( 
     <div className="App">
       {this.state.counters.map(counter => (
-        <Counter key={counter.id}  value={counter.value} id={counter.id} />
+        <Counter 
+        key={counter.id}  
+        onDelete={this.handleDelete}
+        value={counter.value} 
+        id={counter.id} />
       ))}
     </div>
      );
--- src/counter.js
+++ src/counter.js
@@ -25,8 +25,9 @@ class Counter extends Component {
 
         return ( 
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span><br />
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary byn-sm" onClick={this.handleIncrement}>Increment</button>
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)} >Delete</button>
             </div>
          );
     }
